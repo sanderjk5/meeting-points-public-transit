e0af958776cb39ecc4058d299309e5bdcc4e9d29
Jurek Sander <jurek.sander@gmx.de>
2024-01-20 13:53:44 +0100

    feat: init project.

---
+++ src/main.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+
+using namespace std;
+
+static const char *const HEADER = "\nMeeting points - Public Transit\n\n";
+static const char *const USAGE = "\nExecute meeting-points-public-transit\n\n";
+
+int main(int argc, const char *argv[]) {
+  cout << HEADER;
+  cout << USAGE;
+
+  return 0;
+}
