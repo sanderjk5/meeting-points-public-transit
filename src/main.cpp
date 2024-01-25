c81078e1599fb69412ddaf4331c571ca573f2a8a
Jurek Sander <jurek.sander@gmx.de>
2024-01-25 15:02:43 +0100

    feat: added test function.

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,7 @@
 #include<iostream>
 #include<importer.h>
 #include<string>
-#include"meeting-point-algorithms/query-processor.h"
+#include"meeting-point-algorithms/algorithm-tester.h"
 #include"data-handling/converter.h"
 
 
@@ -19,27 +19,7 @@ int main(int argc, const char *argv[]) {
 
   Importer::import(folderName, true, false);
 
-  for (int i = 0; i < 10; i++) {
-    MeetingPointQuery meetingPointQuery = QueryProcessor::generateRandomMeetingPointQuery(5);
-    cout << "Source stops: ";
-    for (int j = 0; j < meetingPointQuery.sourceStopIds.size()-1; j++) {
-      cout << Importer::getStopName(meetingPointQuery.sourceStopIds[j]) << ", ";
-    }
-    cout << Importer::getStopName(meetingPointQuery.sourceStopIds[meetingPointQuery.sourceStopIds.size()-1]);
-    cout << endl;
-    cout << "Source time: " << TimeConverter::convertSecondsToTime(meetingPointQuery.sourceTime, true) << endl;
-    cout << "Weekday: " << WeekdayConverter::convertIntToWeekday(meetingPointQuery.weekday) << endl;
-
-    MeetingPointQueryResult meetingPointQueryResult = QueryProcessor::processNaiveQuery(meetingPointQuery, false);
-    if (meetingPointQueryResult.meetingPointMinSum == "" || meetingPointQueryResult.meetingPointMinMax == "") {
-      cout << "No meeting point found" << endl;
-      cout << "Query time: " << meetingPointQueryResult.queryTime << " milliseconds \n" << endl;
-      continue;
-    }
-    cout << "Min sum - meeting point: " << meetingPointQueryResult.meetingPointMinSum << ", meeting time: " << meetingPointQueryResult.meetingTimeMinSum << ",  travel time sum: " << meetingPointQueryResult.minSumDuration << endl;
-    cout << "Min max - meeting point: " << meetingPointQueryResult.meetingPointMinMax << ", meeting time: " << meetingPointQueryResult.meetingTimeMinMax << ", travel time max: " << meetingPointQueryResult.minMaxDuration << endl;
-    cout << "Query time: " << meetingPointQueryResult.queryTime << " milliseconds \n" << endl;
-  }
+  NaiveAlgorithmTester::testNaiveAlgorithm(10, 3, false);
 
   return 0;
 }
