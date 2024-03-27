753178e5afd910079f403370566efadc75e33feb
sanderjk5 <jurek.sander@gmx.de>
2024-03-27 15:50:36 +0100

    test: changed params.

--- src/main.cpp
+++ src/main.cpp
@@ -93,7 +93,7 @@ int main(int argc, const char *argv[]) {
     // ExperimentController::testAndCompareAlgorithmsRandom(dataType, 1000, numberOfSourceStops);
 
     // Test experiments
-    vector<int> numberOfSourceStops = {2, 5, 10, 25};
+    vector<int> numberOfSourceStops = {2, 5, 10};
 
     ExperimentController::compareRaptorAlgorithms(dataType, 1000, numberOfSourceStops);
 
